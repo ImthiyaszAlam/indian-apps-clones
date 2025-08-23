86a021fa940d632706667b807330c15f12a89b21
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-08-23 23:41:48 +0530

    blinkit splash UI and fade animation in

--- Blinkit/app/src/main/java/com/imthiyas/blinkit/SplashActivity.kt
+++ Blinkit/app/src/main/java/com/imthiyas/blinkit/SplashActivity.kt
@@ -1,20 +1,42 @@
 package com.imthiyas.blinkit
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.animation.AnimationUtils
+import com.imthiyas.blinkit.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val fadeIn = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        binding.splashIcon.startAnimation(fadeIn)
+        binding.splashText.startAnimation(fadeIn)
+
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 200)
+
     }
 }
